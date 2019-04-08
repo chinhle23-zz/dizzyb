b7d126540e12e191d6c1399305aebac2feb16dec
faxedexcel <chinh.le23@gmail.com>
2019-04-08 14:11:24 -0400

    initial commit for heroku

---
+++ dizzyb/heroku_settings.py
@@ -0,0 +1,10 @@
+from freeshelf.settings import *
+   # imports everything from the 'settings.py' file
+import django_heroku
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+   # will prevent specific errors from showing to the production user
+
+django_heroku.settings(locals())
+   # locals returns a dictionary of all your local variables
