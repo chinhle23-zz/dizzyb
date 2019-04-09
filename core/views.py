6d6b756a1b9369af18cebc6678c6feb45e12af40
faxedexcel <chinh.le23@gmail.com>
2019-04-09 05:59:43 -0400

    add gitignore file

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.http import Http404
+
+from core.forms import
+from datetime import date
 
 # Create your views here.
+def index(request):
